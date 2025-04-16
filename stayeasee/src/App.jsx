094146ab12bc658b9e31fccc64eee790aba09612
NeeleshMaurya03 <neeleshmaurya2003@gmail.com>
2025-04-17 00:05:14 +0530

    fx

--- stayeasee/src/App.jsx
+++ stayeasee/src/App.jsx
@@ -2,10 +2,12 @@ import { Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Home from './pages/Home';
 import Host from './pages/Host';
+import About from './pages/About';
 import Contact from './pages/Contact';
 import SignInPage from './pages/SignInPage';
 import SearchPage from './pages/SearchPage';
 import Detail from './pages/ListingDetail';
+import Footer from './pages/Footer';
 
 
 
@@ -19,8 +21,11 @@ function App() {
         <Route path="/contact" element={<Contact />} />
         <Route path="/sign-in" element={<SignInPage />} />
         <Route path="/search" element={<SearchPage />} />
+        <Route path="/about" element={<About />} />
         <Route path="/listing/:id" element={<Detail />} />
+       
       </Routes>
+      <Footer />
     </>
   );
 }
